b206fb585437a2d1ba8ec99b9552cbb44a19a474
www-data <www-data@ecdb.hldns.ru>
2017-04-10 06:50:18 -0400

    Rename some files
    Signed-off-by: www-data <www-data@ecdb.hldns.ru>

---
+++ src/Werd/IotBundle/Entity/Device.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace Werd\IotBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Device
+ *
+ * @ORM\Table(name="device")
+ * @ORM\Entity(repositoryClass="Werd\IotBundle\Repository\DeviceRepository")
+ */
+class Device
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="mac_addr", type="string", length=20, unique=true)
+     */
+    private $macAddr;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="last_seen", type="datetime", nullable=true)
+     */
+    private $lastSeen;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="added_date", type="datetime", nullable=true)
+     */
+    private $addedDate;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Device
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set macAddr
+     *
+     * @param string $macAddr
+     *
+     * @return Device
+     */
+    public function setMacAddr($macAddr)
+    {
+        $this->macAddr = $macAddr;
+
+        return $this;
+    }
+
+    /**
+     * Get macAddr
+     *
+     * @return string
+     */
+    public function getMacAddr()
+    {
+        return $this->macAddr;
+    }
+
+    /**
+     * Set lastSeen
+     *
+     * @param \DateTime $lastSeen
+     *
+     * @return Device
+     */
+    public function setLastSeen($lastSeen)
+    {
+        $this->lastSeen = $lastSeen;
+
+        return $this;
+    }
+
+    /**
+     * Get lastSeen
+     *
+     * @return \DateTime
+     */
+    public function getLastSeen()
+    {
+        return $this->lastSeen;
+    }
+
+    /**
+     * Set addedDate
+     *
+     * @param \DateTime $addedDate
+     *
+     * @return Device
+     */
+    public function setAddedDate($addedDate)
+    {
+        $this->addedDate = $addedDate;
+
+        return $this;
+    }
+
+    /**
+     * Get addedDate
+     *
+     * @return \DateTime
+     */
+    public function getAddedDate()
+    {
+        return $this->addedDate;
+    }
+}
+
