7ea74129917ec8fdc041cb50210360e9a550c39d
Andrey Shamis <lolnik@gmail.com>
2017-09-13 04:49:00 -0700

    Add ResourceBase
    Signed-off-by: Andrey Shamis <lolnik@gmail.com>

---
+++ src/WerdGameBundle/Entity/ResourceBase.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace WerdGameBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+///**
+// * ResourceBase
+// *
+// * @ORM\Table(name="resource_base")
+// * @ORM\Entity(repositoryClass="WerdGameBundle\Repository\DELResourceBaseRepository")
+// */
+class ResourceBase
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer", options={"unsigned"=true})
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255, unique=true)
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return ResourceBase
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
+
