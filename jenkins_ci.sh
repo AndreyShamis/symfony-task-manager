5d2870e2c51c52027491d7877f9ea2a1b192329e
Andrey Shamis <lolnik@gmail.com>
2018-01-26 04:25:23 -0500

    Fix composer autoload psr-4
    Change-Id: Ia8d1fb182b0b5b5e3fe84dcdc2056fe9b7b1f4a4
    Signed-off-by: Andrey Shamis <lolnik@gmail.com>

--- jenkins_ci.sh
+++ jenkins_ci.sh
@@ -1,13 +1,15 @@
+#!/usr/bin/env bash -x
+
 info() {
-  echo "\033[1;33m[Info]    $1  \033[0m"
+  echo -e "\033[1;33m[Info]    $1  \033[0m"
 }
 
 error() {
-  echo "\033[1;31m[Error]   $1  \033[0m"
+  echo -e "\033[1;31m[Error]   $1  \033[0m"
 }
 
 success() {
-  echo "\033[1;32m[Success] $1 \033[0m"
+  echo -e "\033[1;32m[Success] $1 \033[0m"
 }
 
 
@@ -36,8 +38,8 @@ success ""
 #"sudo apt-get install php7.0-xml"
 composer  -vvv update
 info "Check that the composer.json for different errors, like autoload issue:"
-composer validate --no-check-all
-#composer validate --no-check-all --strict  # TODO
+#composer validate --no-check-all
+composer validate --no-check-all --strict  # TODO
 success " ----> Check that the composer.json for different errors, like autoload issue <----"
 info "Check the composer.lock for security issues"
 php vendor/bin/security-checker security:check
