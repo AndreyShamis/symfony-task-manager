f5af6d3dcd5a2471276e510358f05a90af47b71a
Andrey Shamis <lolnik@gmail.com>
2018-02-09 10:37:12 +0200

    Try to add base_urls
    Change-Id: I26fd19618fa9b2fbb5d70e22f48ac042e0d3fa69
    Signed-off-by: Andrey Shamis <lolnik@gmail.com>

--- src/WerdGameBundle/Controller/UserController.php
+++ src/WerdGameBundle/Controller/UserController.php
@@ -5,7 +5,8 @@ namespace WerdGameBundle\Controller;
 use WerdGameBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * User controller.
